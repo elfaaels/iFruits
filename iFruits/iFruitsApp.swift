b93760860858bbdaad5042c62e896008858e7c97
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-01-28 08:58:06 +0700

    Initial Commit

---
+++ iFruits/iFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFruitsApp.swift
+//  iFruits
+//
+//  Created by Elfana Anamta Chatya on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
